fdc778b803cd80a9232208483d9e44088fe2ca89
StasBurov <eagle.heart16@gmail.com>
2022-07-10 13:46:24 +0300

    loops

--- script.js
+++ script.js
@@ -19,7 +19,7 @@
         'logan': '8.1'
     }
 
-Проверить, чтобы все работало без ошибок в консоли */
+Проверить, чтобы все работало без ошибок в консоли 
 
 "use strict";
 
@@ -40,3 +40,60 @@ const a = prompt('Один из последних просмотренных ф
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;      
 console.log(personalMovieDB);
+*/
+
+
+// Место для первой задачи
+function firstTask() {
+    for (let i = 5; i < 11; i++) {
+        console.log(i); // Пишем решение вот тут
+    }   
+}
+firstTask();
+
+// Место для второй задачи
+function secondTask() {
+    for (let i = 20; i > 10; i--) {
+        if (i === 13) {
+            break;
+        }
+        console.log(i); // Пишем решение вот тут
+    }
+}
+secondTask();
+
+// Место для третьей задачи
+function thirdTask() {
+    for (let i = 1; i < 11; i++) {
+        if (i % 2 === 0) {
+            console.log(i);
+        }
+    }
+}
+thirdTask();
+
+// Место для четвертой задачи
+function fourthTask() {
+    let i = 2;
+    while (i <= 16) {
+        if (i % 2 === 0) {
+            i++;
+            continue;
+        } else {
+            console.log(i);
+        }
+        i++;
+    }
+}
+
+// Место для пятой задачи
+function fifthTask() {
+    const arrayOfNumbers = [];
+    for (let i = 5; i < 11; i++) {
+        arrayOfNumbers[i - 5] = i;
+    }
+    console.log(arrayOfNumbers);
+    return arrayOfNumbers;
+}
+
+fifthTask();
